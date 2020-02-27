f0580a79c25e563786d5f8eeef281b6a689ace69
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 19:54:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(path)
     # key #=> "angel"
     # value #=> ["O:)", "☜(⌒▽⌒)☞"]
     new['get_emoticon'][value[0]] = emoticons[key][1]
-    # new['get_emoticon'[value[0]] = 
+    # new['get_emoticon'[value[0]] = "☜(⌒▽⌒)☞"
     new['get_meaning'][value[1]] = key
     binding.pry
   end
