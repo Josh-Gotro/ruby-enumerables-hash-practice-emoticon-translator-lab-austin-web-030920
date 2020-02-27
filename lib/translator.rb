6dbda3a0b579f1957696d4101cb0314f7448fd9d
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 19:59:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # result = load_library(file_path)['get_emoticon'][emoticon]
+  result = load_library(file_path)['get_emoticon'][emoticon]
   # result ? result : "Sorry, that emoticon was not found"
 end
 
