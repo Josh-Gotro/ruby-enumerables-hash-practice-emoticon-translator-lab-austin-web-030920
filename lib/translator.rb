161c5e1cca4be965ecde6d12c31e52de149b82c6
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 19:30:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,32 @@
-# require modules here
+require 'yaml'
+require 'pp'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  #path #=> "./lib/emoticons.yml"
+  emoticons = YAML.load_file(path)
+  # emoticons #=> HoA #=> {"angel"=>["O:)", "☜(⌒▽⌒)☞"],[]...etc}
+  new = {'get_meaning' => {}, 'get_emoticon' => {}}
+
+  emoticons.each do |key,value|
+    new['get_emoticon'][value[0]] = emoticons[key][1]
+    binding.pry
+    new['get_meaning'][value[1]] = key
+  end
+  new
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(hash)
+  japanese_emoticon = []
+  hash.map.values do |value|
+binding.pry
+  end
+  japanese_emoticon
 end
 
 def get_english_meaning
   # code goes here
 end
+
+#ruby-enumerables-hash-practice-emoticon-translator-lab-austin-web-030920/
+# emoticons = YAML.load_file(ruby-enumerables-hash-practice-emoticon-translator-lab-austin-web-030920/lib/emoticons.yml)
