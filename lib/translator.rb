e5fb7b740dd6fd841be92e73cd8e8283862b1fc8
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 20:02:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path)
   new = {'get_meaning' => {}, 'get_emoticon' => {}}
 
   emoticons.each do |key,value|
-                                                      # key #=> "angel"
-                                                     # value #=> ["O:)", "☜(⌒▽⌒)☞"]
+    # key #=> "angel"
+    # value #=> ["O:)", "☜(⌒▽⌒)☞"]
     new['get_emoticon'][value[0]] = emoticons[key][1]
     # new['get_emoticon'[value[0]] = "☜(⌒▽⌒)☞"
     new['get_meaning'][value[1]] = key
